96df0658d51e0f6da42674e87a7be4faba0702e5
Craig Miller <craigmiller160@gmail.com>
2023-07-07 10:22:10 -0400

    Setting up gradle

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val projectGroup: String by project
+val projectVersion: String by project
+
+plugins {
+    id("org.springframework.boot") version "3.0.4"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("io.craigmiller160.gradle.defaults") version "1.2.2"
+    id("com.diffplug.spotless") version "6.17.0"
+}
+
+group = projectGroup
+version = projectVersion
+java.sourceCompatibility = JavaVersion.VERSION_19
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
