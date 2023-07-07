e49123bf60687a8f65e9fcc79978b97f83c9a7e8
Craig Miller <craigmiller160@gmail.com>
2023-07-07 11:24:46 -0400

    Added topic from terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,9 @@ terraform {
 provider "kafka" {
   bootstrap_servers = ["localhost:9092"]
 }
+
+resource "kafka_topic" "tf_topic" {
+  name               = "tf-topic"
+  partitions         = 1
+  replication_factor = 1
+}
