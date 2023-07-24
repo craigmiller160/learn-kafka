4b61443aed2ca64b84357410db904387cc751403
Craig Miller <craigmiller160@gmail.com>
2023-07-24 13:59:40 -0400

    Updated listener

--- src/main/kotlin/io/craigmiller160/learn/kafka/KafkaService.kt
+++ src/main/kotlin/io/craigmiller160/learn/kafka/KafkaService.kt
@@ -6,10 +6,16 @@ import org.springframework.stereotype.Service
 
 @Service
 class KafkaService {
+  private val messages = mutableListOf<Message>()
 
   @KafkaListener(id = "helloTopicGroup", topics = [KafkaConfig.HELLO_TOPIC])
   fun listenToHelloTopic(message: Message) {
     println("RECEIVED MESSAGE: $message")
-    throw RuntimeException("Dying")
+    if ("Fail" == message.body && messages.isEmpty()) {
+      throw RuntimeException("Dying")
+    }
+    messages += message
+    println("ALL MESSAGES")
+    println(messages)
   }
 }
